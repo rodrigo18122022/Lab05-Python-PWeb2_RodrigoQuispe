c92f448043307cfff2cfeb18f09852fb26627359
rodrigo18122022 <roquispehu@unsa.edu.pe>
2024-05-25 21:55:21 -0500

    Parte 2: Ejercicio A

--- Ejercicio2a.py
+++ Ejercicio2a.py
@@ -1,5 +1,7 @@
 from interpreter import draw
 from chessPictures import *
 
-draw(rock.rotate())
- 
+cab_bl = knight
+cab_ng = knight.negative()
+fila = cab_bl.join(cab_ng)
+draw(fila.up(fila.negative()))
